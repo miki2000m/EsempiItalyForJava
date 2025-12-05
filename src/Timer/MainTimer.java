55ff0de7a4e210ca132d2071a6cd3ee222149faf
miki2000m <150129964+miki2000m@users.noreply.github.com>
2025-12-05 10:07:54 +0100

    Refactor timer task scheduling in MainTimer
    Commented out the previous timer and task implementation, and added a more concise approach by directly creating a new TimerTask in the timer.schedule call. Updated delay and period values, and changed the printed message to 'Ciao'.

--- src/Timer/MainTimer.java
+++ src/Timer/MainTimer.java
@@ -8,7 +8,8 @@ public class MainTimer {
         //Timer usa un tempo specifico per eseguire un compito ripetutamente
         //TimerTask è il compito che deve essere eseguito
 
-        Timer timer = new Timer();
+        //Timer timer = new Timer();
+        /*/
         TimerTask task = new TimerTask() {
             //qui ci sarà l'azione da eseguire tramite un semplice override del metodo run() (già presente in TimerTask)
             @Override
@@ -21,6 +22,16 @@ public class MainTimer {
         //task   -> compito da eseguire
         //delay  -> ritardo prima di eseguire il compito (in ms)
         //period -> intervallo di tempo tra un'esecuzione e l'altra (in ms
-        timer.schedule(task, 10000,2000); //stampa "Hello" dopo 0.5 secondi e poi ogni 2 secondi
+        timer.schedule(task, 300,200); //stampa "Hello" dopo 0.5 secondi e poi ogni 2 secondi
+*/
+        //Il metodo più furbo per farlo è così direttamente creando un nuovo timer task nel timer.schedule(new TimerTask(){}, delay, period)
+        Timer timer1= new Timer();
+
+        timer1.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("Ciao");
+            }
+        }, 700,500);
     }
 }
