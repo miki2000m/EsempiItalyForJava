32dcd684349c7166baa848b38aaf0d1b66019d34
miki2000m <150129964+miki2000m@users.noreply.github.com>
2025-12-16 16:29:23 +0100

    Add LinkedList vs ArrayList and linear search examples
    Added LinkListVSArrayList.java to demonstrate performance differences between LinkedList and ArrayList for access and removal operations. Added MainLinearSearch.java to provide a simple implementation of linear search in an array.

---
+++ src/ArrayVettori/Search/MainLinearSearch.java
@@ -0,0 +1,27 @@
+package ArrayVettori.Search;
+
+public class MainLinearSearch {
+    public static void main(String[] args){
+                   //0 1 2 3 4 5 6
+        int[] array={9,1,8,2,7,3,6};
+        int index= linearSearch(array,7);// è posizionato nell'indice 4
+
+        if(index!=-1){
+            System.out.println("Elemento trovato nell'indice: "+index);
+        }
+        else {
+            System.out.println("Elemento non trovato");
+        }
+    }
+
+    private static int linearSearch(int[] array, int value) {
+
+        for(int i=0;i<array.length;i++){
+            if(array[i]==value){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
