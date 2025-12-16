34805e94608ce4d154f86ec5b5d3b8cc2a17e7e8
miki2000m <150129964+miki2000m@users.noreply.github.com>
2025-12-16 16:29:58 +0100

    Add comment explaining linear search performance
    Added a comment in MainLinearSearch.java to clarify that linear search is slow because it iterates through every element in the list.

--- src/ArrayVettori/Search/MainLinearSearch.java
+++ src/ArrayVettori/Search/MainLinearSearch.java
@@ -2,6 +2,8 @@ package ArrayVettori.Search;
 
 public class MainLinearSearch {
     public static void main(String[] args){
+        //Questo tipo di ricerca è lento perché deve iterare ogni elemento della lista
+
                    //0 1 2 3 4 5 6
         int[] array={9,1,8,2,7,3,6};
         int index= linearSearch(array,7);// è posizionato nell'indice 4
