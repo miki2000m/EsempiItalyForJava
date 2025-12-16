1f9e228c3080346b00aa9891ad6a72fe9b4b8d0c
miki2000m <150129964+miki2000m@users.noreply.github.com>
2025-12-16 14:31:24 +0100

    Add examples for reverse priority and string queues
    Expanded MainPriorityQueue with demonstrations of reverse order PriorityQueue for both Double and String types. Improved variable naming for clarity and added comments and print statements to illustrate queue behaviors.

--- src/ArrayVettori/Queue/MainPriorityQueue.java
+++ src/ArrayVettori/Queue/MainPriorityQueue.java
@@ -10,30 +10,70 @@ public class MainPriorityQueue {
         //ovviamente se si aggiunge un cliente cambia l'ultimo cliente
 
         //Permette di dare priorità ai valori più piccoli
-        Queue<Double> queue = new PriorityQueue<>();
+        Queue<Double> queuePriority = new PriorityQueue<>();
 
-        queue.offer(7.5);
-        queue.offer(2.0);
-        queue.offer(3.0);
-        queue.offer(4.0);
+        queuePriority.offer(7.5);
+        queuePriority.offer(2.0);
+        queuePriority.offer(3.0);
+        queuePriority.offer(4.0);
 
         System.out.println("Io sono un priority Queue");
-        while(!queue.isEmpty()){
-            System.out.println(queue.poll());
+        while(!queuePriority.isEmpty()){
+            System.out.println(queuePriority.poll());
         }
 
         //Differenza di stampa
-        Queue<Double> queue2 = new LinkedList<>();
-        queue2.offer(7.5);
-        queue2.offer(2.0);
-        queue2.offer(3.0);
-        queue2.offer(4.0);
+        Queue<Double> queueLinkList = new LinkedList<>();
+
+        queueLinkList.offer(7.5);
+        queueLinkList.offer(2.0);
+        queueLinkList.offer(3.0);
+        queueLinkList.offer(4.0);
 
         System.out.println("\nIo sono un LinkedList Queue");
-        while (!queue2.isEmpty()){
-            System.out.println(queue2.poll());
+        while (!queueLinkList.isEmpty()){
+            System.out.println(queueLinkList.poll());
         }
 
         //Come potete notare stesso ordine di inserimento ma ordine differente di priorità
+
+
+        //Adesso invertiamo l'ordine
+        Queue<Double> queueReversPriority = new PriorityQueue<>(Comparator.reverseOrder());
+
+        queueReversPriority.offer(7.5);
+        queueReversPriority.offer(2.0);
+        queueReversPriority.offer(3.0);
+        queueReversPriority.offer(4.0);
+
+        System.out.println("\nIo sono un reverse Priority Queue");
+        while (!queueReversPriority.isEmpty()){
+            System.out.println(queueReversPriority.poll());
+        }
+
+        //Esempio con String -> ordine alfabetico
+        Queue<String> queueString = new PriorityQueue<>();
+        queueString.offer("B");
+        queueString.offer("C");
+        queueString.offer("A");
+        queueString.offer("D");
+
+        System.out.println("\nIo sono un Priority Queue String Queue ");
+        while (!queueString.isEmpty()){
+            System.out.println(queueString.poll());
+        }
+
+        //Esempio con String invertito -> ordine alfabetico invertito
+        Queue<String> queueStringReverse = new PriorityQueue<>(Comparator.reverseOrder());
+        queueStringReverse.offer("B");
+        queueStringReverse.offer("C");
+        queueStringReverse.offer("F");
+        queueStringReverse.offer("A");
+        queueStringReverse.offer("D");
+
+        System.out.println("\nIo sono un reverse Priority Queue String Queue ");
+        while (!queueStringReverse.isEmpty()){
+            System.out.println(queueStringReverse.poll());
+        }
     }
 }
